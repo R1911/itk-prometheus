d31b9564b055d3504c7270c448a0c2b853d0e3d0
Riivo <riivopikson@gmail.com>
2024-04-27 11:22:38 +0300

    Update index.js

--- srv/nodeSrv/index.js
+++ srv/nodeSrv/index.js
@@ -1,15 +1,81 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+require('dotenv').config();
 
-const port = 9099;
+const PORT = 9099;
+const DISCORD_WEBHOOK_URL = process.env.WEBHOOK_URL;
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post("/alert", (req, res) => {
-	console.log(`incoming request: `, req.body);
+  // debugimiseks ajatempel
+  let timestamp;
+  timestamp = new Date().toLocaleString().replace(",", "");
+
+  // logib terve sissetuleva requesti
+  console.log(`[${timestamp}] Incoming webhook request:`, req.body);
+
+  // dekonstrukteerib sissetuleva requesti, et see ümber ehitada discordile sobivaks
+  const { alerts } = req.body;
+  if (alerts && alerts.length > 0) {
+    const alert = alerts[0];
+    const { status, labels, annotations } = alert;
+    const { alertname, instance, severity } = labels;
+    const { title, description } = annotations;
+
+    let embed = {
+      title: status === "resolved" ? `Teavitus lahendatud` : `Teavitus`,
+      color: status === "resolved" ? 65280 : 16711680,
+      fields: [
+        { name: "Alarmi nimi", value: alertname, inline: true },
+        { name: "Masin", value: instance, inline: true },
+        { name: "Olulisus", value: severity, inline: true },
+        {
+          name: "Kokkuvõte",
+          value: title || "Kokkuvõte puudub",
+          inline: false,
+        },
+        {
+          name: "Kirjeldus",
+          value: description || "Kirjeldus puudub",
+          inline: false,
+        },
+      ],
+      timestamp: new Date().toISOString(),
+    };
+    // edastab ümber tehtud teate discordi
+    request.post(
+      {
+        url: DISCORD_WEBHOOK_URL,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ embeds: [embed] }),
+      },
+      (error, response, body) => {
+        if (error) {
+          timestamp = new Date().toLocaleString().replace(",", "");
+          console.error(
+            `[${timestamp}] Error sending alert to Discord:`,
+            error
+          );
+          res.status(500).send("Error sending alert to Discord");
+        } else {
+          timestamp = new Date().toLocaleString().replace(",", "");
+          console.log(
+            `[${timestamp}] Alert sent to Discord:`,
+            response.statusCode
+          );
+          res.sendStatus(200);
+        }
+      }
+    );
+  } else {
+    res.status(400).send("No alerts found in the request body.");
+  }
 });
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.listen(PORT, () => {
+  timestamp = new Date().toLocaleString().replace(",", "");
+  console.log(`[${timestamp}] Server running on port ${PORT}`);
 });
